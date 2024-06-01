de396697bfe2935a642801ef2d3267485dcb029b
Admin-HL <yeonghaoliang0822@gmail.com>
2024-06-01 14:01:00 +0800

    Initial Commit - Cash Receipt Module Spring Intializr

---
+++ src/test/java/gityeong/CashReceipt/CashReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package gityeong.CashReceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
